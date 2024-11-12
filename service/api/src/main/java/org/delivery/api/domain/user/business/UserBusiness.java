618f9109c1db0f5a030eccd412f6ce7df6936cac
MJ0501 <mjay.do@gmail.com>
2024-11-12 20:36:37 +0900

    jwtodo:Jwt넣기

--- service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -2,8 +2,16 @@ package org.delivery.api.domain.user.business;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
 import org.delivery.api.domain.user.service.UserService;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -11,4 +19,24 @@ import org.delivery.api.domain.user.service.UserService;
 public class UserBusiness {
     private final UserService userService;
     private final UserConverter userConverter;
+    // 사용자 가입처리 로직. request -> entity -> save() -> saved entity -> response -> return response
+    public UserResponse register(UserRegisterRequest request) {
+        var entity = userConverter.toEntity(request);
+        var newEntity = userService.register(entity);
+        var response = userConverter.toResponse(newEntity);
+        return response;
+//        return Optional.ofNullable(request)
+//                .map(userConverter::toEntity)
+//                .map(userService::register)
+//                .map(userConverter::toResponse)
+//                .orElseThrow(()->new ApiException(ErrorCode.NULL_POINT,"request NULL"));
+    }
+    // 로그인.
+    // 사용자체크(email,password) -> user Entity 로그인 확인 -> token 생성 -> token response
+    public UserResponse login(UserLoginRequest request) {
+        var userEntity = userService.login(request.getEmail(),request.getPassword());
+        // 사용자 없으면 throw
+        // TODO 토큰 생성 (사용자 있다면)
+        return userConverter.toResponse(userEntity);
+    }
 }
--- service/api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ service/api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -1,10 +1,39 @@
 package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.db.user.UserEntity;
 import org.delivery.db.user.UserRepository;
+import org.delivery.db.user.enums.UserStatus;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+// user domain 로직 처리 하는 서비스
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    // userEntity 가 들어오고 나간다!
+    public UserEntity register(UserEntity userEntity){
+        return Optional.ofNullable(userEntity).map(it -> {
+            userEntity.setStatus(UserStatus.REGISTERED);
+            userEntity.setRegistered_at(LocalDateTime.now());
+            return userRepository.save(userEntity);
+        }).orElseThrow(()->new ApiException(ErrorCode.NULL_POINT,"User Entity NULL"));
+    }
+    public UserEntity login(String email, String password){
+        var entity = getUserWithThrow(email, password);
+        return entity;
+    }
+    public UserEntity getUserWithThrow(
+            String email, String password
+    ){
+        return userRepository.findFirstByEmailAndPasswordAndStatusOrderByIdDesc(
+                email,password,UserStatus.REGISTERED
+        ).orElseThrow(()->new ApiException(UserErrorCode.USER_NOT_FOUND));
+    }
 }
