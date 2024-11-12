19e151a8e97095b633a736b39f605320d5e97d06
MJ0501 <mjay.do@gmail.com>
2024-11-12 15:21:47 +0900

    사용자로직기본ìpackage설정

--- service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -2,7 +2,13 @@ package org.delivery.api.domain.user.business;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
+import org.delivery.api.domain.user.converter.UserConverter;
+import org.delivery.api.domain.user.service.UserService;
+
+
 @RequiredArgsConstructor
 @Business
 public class UserBusiness {
+    private final UserService userService;
+    private final UserConverter userConverter;
 }
--- service/api/src/main/java/org/delivery/api/domain/user/service/UserService.java
+++ service/api/src/main/java/org/delivery/api/domain/user/service/UserService.java
@@ -1,8 +1,10 @@
 package org.delivery.api.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.db.user.UserRepository;
 import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 }
